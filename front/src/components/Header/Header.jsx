0be4f74560e8143fcb4c91323774d4bd80c4f33b
Ramyy220 <ramyamr70@gmail.com>
2024-04-22 14:39:45 +0200

    nav v2

--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -5,38 +5,44 @@ import logo from '../../assets/images/Logo.jpeg';
 import { Container, Row } from 'reactstrap';
 
 const Header = () => {
-  return <header className="header">
+  return (
+    <header className="header">
       <Container>
         <Row>
           <div className="nav__wrapper">
-          <div className="logo">
-            <img src={logo} alt="logo" />
-          </div>
-          <div className="navigation">
-            <ul className="menu">
-              <li className="nav__item"> 
-                <NavLink to="Home" exact>HOME</NavLink>
-              </li>
-              <li className="nav__item">
-                <NavLink to="Services" exact>SERVICES</NavLink>
-              </li>
-              <li className="nav__item"> 
-                <NavLink to="Contact" exact>CONTACT</NavLink>
-              </li>
-            </ul>
+            <div className="logo">
+              <img src={logo} alt="logo" />
             </div>
-        <div className="nav__icons">
-          <span className="user__icon">
-            <i className="ri-shield-user-line"></i> 
-          </span>
-          <div className="mobile__menu">
-            <i className="ri-menu-line"></i> 
+            <div className="navigation">
+              <ul className="menu">
+                <li className="nav__item"> 
+                  <NavLink to="Home" exact>HOME</NavLink>
+                </li>
+                <li className="nav__item">
+                  <NavLink to="Services" exact>SERVICES</NavLink>
+                </li>
+                <li className="nav__item"> 
+                  <NavLink to="Contact" exact>CONTACT</NavLink>
+                </li>
+              </ul>
+            </div>
+            <div className="nav__buttons">
+              <button className="connection__button">Connexion</button>
+              <button className="inscription__button">Inscription</button>
+            </div>
+            <div className="nav__icons">
+              <span className="user__icon">
+                <i className="ri-shield-user-line"></i> 
+              </span>
+              <div className="mobile__menu">
+                <i className="ri-menu-line"></i> 
+              </div>
+            </div>
           </div>
-        </div>
-        </div>
         </Row>
       </Container>
     </header>
+  );
 }
 
 export default Header;
